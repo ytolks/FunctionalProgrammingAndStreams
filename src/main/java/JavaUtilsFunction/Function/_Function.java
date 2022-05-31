77eccf699336c280cd56a4dd482769b46aaccc23
ytolks <yevgeniy.tolks@gmail.com>
2022-05-31 16:38:19 +0300

    commit

--- src/main/java/JavaUtilsFunction/Function/_Function.java
+++ src/main/java/JavaUtilsFunction/Function/_Function.java
@@ -1,7 +1,11 @@
 package JavaUtilsFunction.Function;
 
+import java.util.function.Function;
+
 public class _Function {
     public static void main(String[] args) {
-        
+        System.out.println(incrementBy2.apply(1));
     }
+
+    static Function<Integer, Integer> incrementBy2 = number -> number + 2;
 }
