1e46f36c972612f7dd3306913e5eaf1c176b672f
ytolks <yevgeniy.tolks@gmail.com>
2022-05-31 18:28:22 +0300

    commit

---
+++ src/main/java/JavaUtilsFunction/Function/_BiFunction.java
@@ -0,0 +1,22 @@
+package JavaUtilsFunction.Function;
+
+import java.math.BigDecimal;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+public class _BiFunction {
+    public static void main(String[] args) {
+        System.out.println(sumOfTwoNumbers.apply(1, 10));
+        System.out.println(multiplyTwoNumbers.apply(2, 12));
+        System.out.println(sumTwoNumbersAndThenIncrementBy3.apply(3, 3));
+    }
+
+    // BiFunction 2 argument and 1 result
+    static Function<Integer, Integer> multiplyBy3 = (number) -> number * 3;
+    static BiFunction<Integer, Integer, Integer> sumOfTwoNumbers = (number, number1) -> number + number1;
+
+    static BiFunction<Integer, Integer, Integer> multiplyTwoNumbers = (number, number1) -> number * number1;
+    static BiFunction<Integer, Integer, Integer> sumTwoNumbersAndThenIncrementBy3 =
+            sumOfTwoNumbers.andThen(multiplyBy3);
+
+}
--- src/main/java/JavaUtilsFunction/Function/_Function.java
+++ src/main/java/JavaUtilsFunction/Function/_Function.java
@@ -5,7 +5,12 @@ import java.util.function.Function;
 public class _Function {
     public static void main(String[] args) {
         System.out.println(incrementBy2.apply(1));
+        System.out.println(multiplyBy9.apply(1));
+        System.out.println(incrementBy2AndMultiplyBy9.apply(1));
     }
-
+// Function 1 argument and 1 result
     static Function<Integer, Integer> incrementBy2 = number -> number + 2;
+    static Function<Integer, Integer> multiplyBy9 = number -> number * 9;
+static Function<Integer,Integer> incrementBy2AndMultiplyBy9 =
+        incrementBy2.andThen(multiplyBy9);
 }
